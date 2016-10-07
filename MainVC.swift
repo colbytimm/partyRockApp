bc1798e0d516b5782e60250132e385d76c569a09
colbytimm <timmcolby@hotmail.com>
2016-10-06 21:50:27 -0700

    First commit

---
+++ MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  partyRockApp
+//
+//  Created by Colby Timm on 2016-10-06.
+//  Copyright © 2016 Colby Timm. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
